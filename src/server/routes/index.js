5c8a4c8507e206124b8852553da05ca74baf8067
Marlon Carvalho <53587791+projetoMuseu@users.noreply.github.com>
2024-02-01 02:02:53 -0300

    Add files via upload

---
+++ src/server/routes/index.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import puppeteer from "puppeteer";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send("Olá, DEV!");
+});
+
+router.post("/pdf", async (req, res) => {
+  const { htmlContent } = req.body;
+  
+  if (!htmlContent) {
+    return res.status(400).send("HTML content is required");
+  }
+
+  try {
+    const browser = await puppeteer.launch({ headless: 'new' });
+    const page = await browser.newPage();
+    
+    await page.setContent(htmlContent);
+
+    await page.emulateMediaType('screen');
+
+    const pdf = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+
+    res.contentType("application/pdf");
+
+    return res.send(pdf);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+export { router };
