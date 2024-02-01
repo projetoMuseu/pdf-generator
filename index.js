638dca16ae0593f6d2685ce9a260e0fc249ad17e
Marlon Carvalho <53587791+projetoMuseu@users.noreply.github.com>
2024-02-01 14:14:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ import {router} from './src/routes/index.js';
 
 const server = express();
 server.use(cors());
+server.use((req, res)=> {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 server.use(express.json({ limit: '50mb' }));
 server.use(express.urlencoded({ limit: '50mb', extended: true }));
